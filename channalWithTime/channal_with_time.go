d418e332692c6a08ba0a3dae8732286bdc21b8be
Мармулев Семён <marmulev2003@mail.ru>
2025-06-02 13:41:54 +0300

    change task

--- channalWithTime/channal_with_time.go
+++ channalWithTime/channal_with_time.go
@@ -2,26 +2,54 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+// если в первая горутина положит в канал значение, и в это же время закроется канал ch_stop,
+// то вторая горутина завершится и не будет забирать значение из канала, поэтому первая будет ждать,
+// когда у неё заберут значение, а горутина main будет ждать завершения первой горутины (wg.Wait())
+// можно убрать wg.Wait(), либо сделать буффер, тогда первая положит в буфер и сможет завершиться, либо как-то по-другому работать с select
 func main() {
 	ch_stop := make(chan int)
-
-	go func_with_somthing_work(ch_stop) //запустили работу с каналом
-
-	time.Sleep(4 * time.Second) // ждем какоете врямя
+	ch := make(chan int, 1)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go sentToChan(ch, ch_stop, &wg)
+	go takeFromchan(ch, ch_stop, &wg)
+	time.Sleep(1 * time.Second) // ждем какоето врямя
 	close(ch_stop)
+	wg.Wait()
 }
-func func_with_somthing_work(ch chan int) {
-	tick := time.NewTicker(time.Second)
+func sentToChan(ch chan int, stop_ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	//defer close(ch)
+
+	tick := time.NewTicker(3 * time.Millisecond)
+
 	i := 1
 	for {
 		select {
 		case <-tick.C:
-			fmt.Printf("%d я нашел себе ррработку\nбуду чистить сковорррродку\n", i)
+			//fmt.Printf("%d я нашел себе ррработку\nбуду чистить сковорррродку\n", i)
+			ch <- i
 			i++
-		case <-ch:
+		case <-stop_ch:
+			//tick.Stop()
+			return
+		}
+	}
+}
+func takeFromchan(ch chan int, stop_ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case i, ok := <-ch:
+			if !ok {
+				return
+			}
+			fmt.Printf("%d пришло\n", i)
+		case <-stop_ch:
 			return
 		}
 	}
